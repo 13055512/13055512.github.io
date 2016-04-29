b28bff79c74d28b16423f3ee8decdb832facdcdc
Ray <13055512@hdu.edu.cn>
2016-04-29 20:51:30 +0800

    添加文章目录

--- js/page.js
+++ js/page.js
@@ -1,7 +1,16 @@
-$('#test').click(function(){
-    var titles = $('h2').not(".null").text().alert;
-    var h2;
-    for (var title in titles) {
-        
+$(document).ready(function(){
+    var hd = $('h2').not(".null");
+    var num = hd.size();
+    if (num > 0) {
+        var id = hd.first().attr('id');
+        var text = hd.first().text();
+        text = '<li id="id' + id + '"><a class="active">' + text + '</a></li>';
+        $('ul#content').append(text);
+    for (var i = 1; i < num; i++){
+        var id = hd.eq(i).attr('id');
+        var text = hd.eq(i).text();
+        text = '<li id="id' + id + '"><a class="disactive">' + text + '</a></li>';
+        $('ul#content').append(text);
     }
+    };
 });
