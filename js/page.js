cf20a6ce9ca3a27c67ef6449294bb4b9b067f8c8
Ray <13055512@hdu.edu.cn>
2016-05-02 13:48:25 +0800

    目录跳转
    滚动监听还没弄

--- js/page.js
+++ js/page.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
     var hd = $('h2').not(".null");
     var num = hd.size();
-    if (num > 0) {
-        var id = hd.first().attr('id');
-        var text = hd.first().text();
-        text = '<li id="id' + id + '"><a class="active">' + text + '</a></li>';
-        $('ul#content').append(text);
-    for (var i = 1; i < num; i++){
+    for (var i = 0; i < num; i++){
         var id = hd.eq(i).attr('id');
         var text = hd.eq(i).text();
-        text = '<li id="id' + id + '"><a class="disactive">' + text + '</a></li>';
+        text = '<li><a href="#' + id + '">' + text + '</a></li>';
         $('ul#content').append(text);
+        hd.eq(i).attr("class", "section scrollspy");
     }
-    };
+    $('.scrollspy').scrollSpy();
 });
